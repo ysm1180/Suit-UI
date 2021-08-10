b902e0e21157d6bef52fae146649afe136c8d723
ysm1180 <ysm1180@github.com>
2021-08-10 20:14:58 +0900

    Rename `ErrorToast` to `ErrorDialog`

---
+++ test/ErrorDialog.test.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { ErrorDialog } from '../src';
+import mountTest from '../tests/mountTest';
+
+describe('ErrorDialog', () => {
+    mountTest(<ErrorDialog />);
+});
--- test/ErrorToast.test.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { ErrorToast } from '../src';
-import mountTest from '../tests/mountTest';
-
-describe('ErrorToast', () => {
-    mountTest(<ErrorToast />);
-});
